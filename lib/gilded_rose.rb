d44dcf1cc342b1dba7a8e09ed9245743b386d91a
Kareem <kareem335@yahoo.co.uk>
2019-08-29 10:59:40 +0100

    extracted items into seperate file

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,11 +1,14 @@
+require_relative 'item'
+
 class GildedRose
 
     def initialize(items)
       @items = items
     end
   
-    def update_quality()
+    def update_quality
       @items.each do |item|
+        p item
         if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
           if item.quality > 0
             if item.name != "Sulfuras, Hand of Ragnaros"
@@ -50,19 +53,5 @@ class GildedRose
           end
         end
       end
-    end
-  end
-  
-  class Item
-    attr_accessor :name, :sell_in, :quality
-  
-    def initialize(name, sell_in, quality)
-      @name = name
-      @sell_in = sell_in
-      @quality = quality
-    end
-  
-    def to_s()
-      "#{@name}, #{@sell_in}, #{@quality}"
     end
   end
